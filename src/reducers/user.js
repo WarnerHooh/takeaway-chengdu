b4f6761066bccc8371663c2ed32e27ce967e74ed
Lei Zeng <leizeng@CNleizeng-2.local>
2016-07-11 17:20:06 +0800

    add user mock and reducer

---
+++ src/reducers/user.js
@@ -0,0 +1,10 @@
+import { handleActions } from 'redux-actions'
+
+import { USER_LOGIN, USER_LOGOUT } from '../actions/user'
+
+export default handleActions({
+  [USER_LOGIN]: (state = {}, { token }) => ({ ...state, token }),
+  [USER_LOGOUT]: (state = {}) => ({ ...state, token: null })
+}, {
+  token: null
+})
