b7bb1ab3ce5ee80daea79ec48da4f8b7f690aa27
miyizs <yinzhaoshu90@gmail.com>
2016-07-11 22:00:49 +0800

    fix problem

--- src/containers/HomePage.js
+++ src/containers/HomePage.js
@@ -2,8 +2,6 @@ import React, { Component, PropTypes } from 'react'
 import { connect } from 'react-redux'
 import RestarantsContainer from '../containers/RestarantsContainer.js'
 
-import Button from '../components/Button'
-
 class HomePage extends Component {
   static propTypes = {
     dispatch: PropTypes.func.isRequired,
